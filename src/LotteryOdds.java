e8832ab6122019ee31d57fac686793938430b86b
tmjames1977 <33112355+tmjames1977@users.noreply.github.com>
2017-11-05 23:58:16 -0600

    Changes to Strings

--- src/LotteryOdds.java
+++ src/LotteryOdds.java
@@ -1,27 +1,27 @@
 import java.math.BigInteger;
 
 public class LotteryOdds {
-
+	
 	public static void main(String[] args) {
 
 		int r;
 		int w = 10; // 69
 		int s = 2; // 5
-		int pb = 26;
-	    r = factorial(w) / (factorial(s) * (factorial(w-s)));  // Superduper magic equation!!
-	    r = r * pb;
+		BigInteger pb = new BigInteger = ("26");
+	    r = factorial(w).divide (factorial(s).multiply (factorial(w-s)));  // Superduper magic equation!!
+	    r = r.multiply (pb);
 	    
 	    System.out.println("The odds of winning the PowerBall are 1:" + r);
 	    System.out.println("The PowerBall website says that it's 1:292201338");
 	    
     }
 
-	private static Integer factorial(int n) {
-	    int i = 1;
-	    int f = 1;
+	private static BigInteger factorial(int n) {
+	    long i = 1;
+	    BigInteger f = new BigInteger("1");
 	 
-	    for (int c = 1; c <= n; c++) {
-	      f = f * i++;
+	    for (int i1 = 1; i1 <= n; i1++) {
+	      f = f.multiply(BigInteger.valueOf(i1));
 	    }
 
 		return f;
